17751065c5fde3f4e0c79fa0b1660b44ff4cced2
ski-freak <mewhocantfindanickname@gmail.com>
2025-10-14 17:27:05 -0500

    laptop commit

--- scenes/entities/player/FALL.gd
+++ scenes/entities/player/FALL.gd
@@ -1,5 +1,9 @@
 extends "state.gd"
 
+@onready var CoyoteTimer = $CoyoteTimer
+@export var coyote_duration = 0.2
+
+var can_jump = true
 
 func update(delta):
 	Player.gravity(delta)
@@ -10,4 +14,15 @@ func update(delta):
 		return STATES.DASH
 	if Player.get_next_to_wall() != null:
 		return STATES.WALLSLIDE
+	if Player.jump_input_actuation and can_jump:
+		return STATES.JUMP
 	return null
+
+func enter_state():
+	if Player.prev_state == STATES.IDLE or Player.prev_state == STATES.MOVE or Player.prev_state == STATES.SLIDE:
+		can_jump = true
+		CoyoteTimer.start(coyote_duration)
+	else:
+		can_jump = false
+
+	pass
